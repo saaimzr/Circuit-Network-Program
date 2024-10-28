1d145f492994c06b4d2bb5dc5d34569ffdf5d400
saaimzr <153339472+saaimzr@users.noreply.github.com>
2024-10-27 20:24:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,4 @@
-//
-//  main.cpp
-//  Lab 3 The Resistor Network Program
-//
-//  Created by Nathan Hung on 2024-09-11.
-//  Modified by Salma Emara on 2024-09-30
-//
-//  Permission is hereby granted to use this code in ECE244 at
-//  the University of Toronto. It is prohibited to distribute
-//  this code, either publicly or to third parties.
-//
-//  ECE244 Student: Complete the skeletal code of the main function in this file
+
 
 #include <iomanip>
 #include <iostream>
